0525d69267ee2224c59305edf36ce828c1a6f585
turnoxok <turnoxok@gmail.com>
2026-02-19 08:53:09 -0300

    Update send-push.js

--- netlify/functions/send-push.js
+++ netlify/functions/send-push.js
@@ -28,7 +28,7 @@ exports.handler = async (event, context) => {
     );
 
     // Obtener suscripción del usuario desde Apps Script
-    const SHEETS_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbwG8rCodce4HeWAp1uHMjBIU64Bk_Sds3Dx9laGN0oCLNJUy2130deMjjp5CvxrmXvs/exec";
+    const SHEETS_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbyCqxu6i_rZ92jcVDeJpKp3-12nByWVWab3JSUtoW1Xso6I8q899yEAM0_mJLQD5P6D/exec";
     
     const res = await fetch(SHEETS_WEBAPP_URL, {
       method: "POST",
