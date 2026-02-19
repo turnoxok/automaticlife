e3a75ce3d1f72533368032f02ea2bad18d6c27d1
turnoxok <turnoxok@gmail.com>
2026-02-19 10:25:39 -0300

    Update respond.js

--- netlify/functions/respond.js
+++ netlify/functions/respond.js
@@ -1,7 +1,7 @@
 // Netlify Function - CommonJS format
 const fetch = require("node-fetch");
 const { OpenAI } = require("openai");
-const SHEETS_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbzAsT0vBbZSVlm7Y_D9v-W9Kaw7EYjAuqnXYJDubWdihNZwIxqOkRpbskFnudVscMsM/exec";
+const SHEETS_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbwePAxunLN17i3WZ13C-VxgVv1U43adYZLkO8r0xA42svve7jale9Gj0OlIYzkVdgk/exec";
 exports.handler = async (event, context) => {
   const headers = {
     "Access-Control-Allow-Origin": "*",
